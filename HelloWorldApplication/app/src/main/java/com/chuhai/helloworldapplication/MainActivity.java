24e6b9b871596d3211c34719783aafb4e3e817ef
unknown <wk75548305@gmail.com>
2019-09-19 17:08:37 +0800

    java ex 1 finished

--- HelloWorldApplication/app/src/main/java/com/chuhai/helloworldapplication/MainActivity.java
+++ HelloWorldApplication/app/src/main/java/com/chuhai/helloworldapplication/MainActivity.java
@@ -4,16 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+/*import android.widget.TextView;*/
+import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.Random;
+/*import java.util.Random;*/
 
 public class MainActivity extends AppCompatActivity {
 
     // Sets up initial points and random numbers on the buttons.
+
+    private Button leftbutton;
+    private Button rightbutton;
+    private int score = 0;
+    private int leftvalue = 0;
+    private int rightvalue = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        score = 0;
+        pickRandomNumbers();
+
 
         // TODO: initialize the point to 0,
         //       and call function pickRandomNumbers
@@ -22,20 +37,32 @@ public class MainActivity extends AppCompatActivity {
     private void pickRandomNumbers() {
         // TODO: Chooses two random numbers from 0-9 and
         //       places them on the left/right buttons.
-        Random rand = new Random();
-        int n = rand.nextInt(50);
+        leftbutton = findViewById(R.id.theleftbutton);
+        rightbutton = findViewById(R.id.therightbutton);
+
+        do {
+            leftvalue = (int) (Math.random() * (10));
+            rightvalue = (int) (Math.random() * (10));
+        } while (leftvalue == rightvalue);
+
+        leftbutton.setText(String.valueOf(leftvalue));
+        rightbutton.setText(String.valueOf(rightvalue));
     }
 
     // Called when the left number button is clicked.
     public void leftButtonClick(View view) {
         // TODO: Call the function buttonClickHelper and
         //       specify isLeft = true
+        boolean isLeft = true;
+        buttonClickHelper(isLeft);
     }
 
     // Called when the right number button is clicked.
     public void rightButtonClick(View view) {
         // TODO: Call the function buttonClickHelper and
         //       specify isLeft = false
+        boolean isLeft = false;
+        buttonClickHelper(isLeft);
     }
 
     // A helper to handle a click on the left or right button,
@@ -47,10 +74,42 @@ public class MainActivity extends AppCompatActivity {
         // If larger, increment the point by 1, and show the message “Great Job”
         // Otherwise, decrement the point by 1, and show the message “Wrong ans”
 
+
+
+        if(isLeft == true){
+
+            if (leftvalue > rightvalue){
+                Toast.makeText(this, "Great Job", Toast.LENGTH_SHORT).show();
+                score ++;
+            }
+            else {
+                Toast.makeText(this, "nah", Toast.LENGTH_SHORT).show();
+                score --;
+            }
+        }
+
+
+        if(isLeft == false){
+
+            if (leftvalue < rightvalue){
+                Toast.makeText(this, "Great Job", Toast.LENGTH_SHORT).show();
+                score ++;
+            }
+            else {
+                Toast.makeText(this, "nah", Toast.LENGTH_SHORT).show();
+                score --;
+
+                if(score < 0)
+                    score = 0;
+            }
+        }
         // TODO: update display of points
 
+        ((TextView)findViewById(R.id.scores)).setText(String.valueOf(score));
 
         // TODO: choose two new random numbers for next round
+
+        pickRandomNumbers();
     }
 
 }
