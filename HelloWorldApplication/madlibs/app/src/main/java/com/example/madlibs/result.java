186c0b4ff6ae743c650f9ed6233e7ef7502ed98c
wk75548305 <54925403+wk75548305@users.noreply.github.com>
2019-11-25 22:21:49 +0900

    Mobile A2

--- HelloWorldApplication/madlibs/app/src/main/java/com/example/madlibs/result.java
+++ HelloWorldApplication/madlibs/app/src/main/java/com/example/madlibs/result.java
@@ -10,5 +10,7 @@ public class result extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+
     }
 }
